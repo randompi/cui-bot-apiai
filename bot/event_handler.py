9b9b0d9c8760142f3a39788f04624b25fa71f4cc
Randall Barnhart <randall.barnhart@robotsandpencils.com>
2016-04-22 16:22:48 -0600

    Modified to use my wit.ai app context, encoded normal values table, and handling new story to lookup normal values.

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -1,10 +1,95 @@
 import json
 import logging
 import re
+import sys
 from wit import Wit
 
 # TODO: make this configurable
-access_token = '3ASGKNIRJ4U6PDQ66SYNLYCSLEQIGZWD'
+# access_token = '3ASGKNIRJ4U6PDQ66SYNLYCSLEQIGZWD' # kcherniwchan / My First App
+access_token = 'T524XQBBYKKUSVTKG4US7B5KXJJINJHY' # randompi / cui-test
+
+
+# TODO: Read these in from editable files
+# 'value': , 'sd': , 'range_lower': , 'range_upper':
+abs_norm_values = {
+    'male':{
+        'lt35':{
+            'lv':{
+                'edv':{'value': 173, 'sd': 29, 'range_lower': 115, 'range_upper': 231},
+                'esv':{'value': -1, 'sd': 15, 'range_lower': 27, 'range_upper': 87},
+                'sv':{'value': 118, 'sd': 18, 'range_lower': 82, 'range_upper': 154},
+                'ef':{'value': 67, 'sd': 5, 'range_lower': 57, 'range_upper': 77},
+                'mass':{'value': 131, 'sd': 21, 'range_lower': 89, 'range_upper': 173},
+            },
+            'rv': {
+                'edv': {'value': 203, 'sd': 33, 'range_lower': 137, 'range_upper': 269},
+                'esv': {'value': 87, 'sd': 20, 'range_lower': 47, 'range_upper': 127},
+                'sv': {'value': 116, 'sd': 19, 'range_lower': 78, 'range_upper': 154},
+                'ef': {'value': 57, 'sd': 5, 'range_lower': 47, 'range_upper': 67},
+                'mass': {'value': 42, 'sd': 8, 'range_lower': 26, 'range_upper': 58},
+            },
+        },
+        'gte35': {
+            'lv': {
+                'edv': {'value': 149, 'sd': 25, 'range_lower': 99, 'range_upper': 199},
+                'esv': {'value': 43, 'sd': 13, 'range_lower': 17, 'range_upper': 69},
+                'sv': {'value': 106, 'sd': 19, 'range_lower': 68, 'range_upper': 144},
+                'ef': {'value': 71, 'sd': 6, 'range_lower': 59, 'range_upper': 83},
+                'mass': {'value': 120, 'sd': 23, 'range_lower': 74, 'range_upper': 166},
+            },
+            'rv': {
+                'edv': {'value': 181, 'sd': 28, 'range_lower': 125, 'range_upper': 237},
+                'esv': {'value': 71, 'sd': 17, 'range_lower': 37, 'range_upper': 105},
+                'sv': {'value': 110, 'sd': 18, 'range_lower': 74, 'range_upper': 146},
+                'ef': {'value': 61, 'sd': 6, 'range_lower': 49, 'range_upper': 73},
+                'mass': {'value': 39, 'sd': 7, 'range_lower': 25, 'range_upper': 53},
+            },
+        },
+    },
+    'female': {
+        'lt35': {
+            'lv': {
+                'edv': {'value': 137, 'sd': 25, 'range_lower': 87, 'range_upper': 187},
+                'esv': {'value': 43, 'sd': 11, 'range_lower': 21, 'range_upper': 65},
+                'sv': {'value': 96, 'sd': 18, 'range_lower': 60, 'range_upper': 132},
+                'ef': {'value': 69, 'sd': 6, 'range_lower': 57, 'range_upper': 81},
+                'mass': {'value': 92, 'sd': 20, 'range_lower': 52, 'range_upper': 132},
+            },
+            'rv': {
+                'edv': {'value': 152, 'sd': 27, 'range_lower': 98, 'range_upper': 206},
+                'esv': {'value': 59, 'sd': 12, 'range_lower': 35, 'range_upper': 83},
+                'sv': {'value': 93, 'sd': 17, 'range_lower': 59, 'range_upper': 127},
+                'ef': {'value': 61, 'sd': 3, 'range_lower': 55, 'range_upper': 67},
+                'mass': {'value': 36, 'sd': 7, 'range_lower': 22, 'range_upper': 50},
+            },
+        },
+        'gte35': {
+            'lv': {
+                'edv': {'value': 128, 'sd': 23, 'range_lower': 82, 'range_upper': 174},
+                'esv': {'value': 40, 'sd': 12, 'range_lower': 16, 'range_upper': 64},
+                'sv': {'value': 89, 'sd': 16, 'range_lower': 57, 'range_upper': 121},
+                'ef': {'value': 69, 'sd': 6, 'range_lower': 57, 'range_upper': 81},
+                'mass': {'value': 92, 'sd': 19, 'range_lower': 54, 'range_upper': 130},
+            },
+            'rv': {
+                'edv': {'value': 140, 'sd': 37, 'range_lower': 66, 'range_upper': 214},
+                'esv': {'value': 52, 'sd': 22, 'range_lower': 8, 'range_upper': 96},
+                'sv': {'value': 93, 'sd': 17, 'range_lower': 50, 'range_upper': 126},
+                'ef': {'value': 64, 'sd': 7, 'range_lower': 50, 'range_upper': 78},
+                'mass': {'value': 33, 'sd': 7, 'range_lower': 19, 'range_upper': 47},
+            },
+        },
+    },
+    'units':{
+        'edv': 'ml',
+        'esv': 'ml',
+        'sv': 'ml',
+        'ef': '%',
+        'mass': 'g',
+        'bsa': 'm^2',
+    },
+}
+
 
 logger = logging.getLogger(__name__)
 
@@ -26,8 +111,11 @@ class RtmEventHandler(object):
             'say': self.say,
             'merge': self.merge,
             'error': self.error,
+            'lookupHCV': self.lookupHCV,
+            'lookupNormalValue': self.lookupNormalValue,
         }
         self.wit_client = Wit(access_token, actions)
+        self.context = {}
 
     def say(self, session_id, context, msg):
         logger.debug('say:: session_id: {}, msg: {}'.format(session_id, msg))
@@ -36,9 +124,59 @@ class RtmEventHandler(object):
 
     def merge(self, session_id, context, entities, msg):
         logger.debug('merge:: session_id: {}, context: {}, entities:{}, msg:{}'.format(session_id, context, entities, msg))
-        hrt_segs = first_entity_value(entities, 'heart_segments')
-        if hrt_segs:
-            context['heart_segments'] = hrt_segs
+        hcv_val = first_entity_value(entities, 'heart_chamber_value')
+        if hcv_val:
+            context['hcv'] = hcv_val.lower()
+        gender = first_entity_value(entities, 'gender')
+        if gender:
+            context['gender'] = gender.lower()
+        age = first_entity_value(entities, 'age_of_person')
+        if age:
+            age_nums = re.findall('\d+', age)
+            if len(age_nums) > 0:
+                context['age'] = age_nums[0]
+        ventricle = first_entity_value(entities, 'ventricle_type')
+        if ventricle:
+            context['ventricle'] = ventricle.lower()
+
+        logger.debug('end merge:: context: {}'.format(context))
+        return context
+
+    def lookupNormalValue(self, session_id, context):
+        logger.debug('lookupNormalValue:: session_id: {}, context: {}'.format(session_id, context))
+        if 'hcv' in context and 'gender' in context and 'age' in context and 'ventricle' in context:
+
+            if context['age'] < 35:
+                age_key = 'lt35'
+            else:
+                age_key = 'gte35'
+
+            spec_norm_vals = abs_norm_values[context['gender']][age_key][context['ventricle']][context['hcv']]
+            context['norm_val'] = spec_norm_vals['value']
+            context['std_val'] = spec_norm_vals['sd']
+            context['range_lower'] = spec_norm_vals['range_lower']
+            context['range_upper'] = spec_norm_vals['range_upper']
+            context['hcv_units'] = abs_norm_values['units'][context['hcv']]
+
+        return context
+
+    def lookupHCV(self, session_id, context):
+        logger.debug('lookupHCV:: session_id: {}, context: {}'.format(session_id, context))
+        if 'hcv' in context:
+            hcv_val = context['hcv'].lower()
+            if hcv_val == 'edv':
+                context['hcv_meaning'] = '_end-diastolic volume_'
+            elif hcv_val == 'esv':
+                context['hcv_meaning'] = '_end-systolic volume_'
+            elif hcv_val == 'ef':
+                context['hcv_meaning'] = '_ejection fraction_'
+            elif hcv_val == 'sv':
+                context['hcv_meaning'] = '_stroke volume_'
+            elif hcv_val == 'mass':
+                context['hcv_meaning'] = '_myocardial mass_'
+            else:
+                context['hcv_meaning'] = 'unknown'
+
         return context
 
     def error(self, session_id, context, e):
@@ -69,7 +207,7 @@ class RtmEventHandler(object):
 
     def _handle_message(self, event):
         # Filter out messages from the bot itself
-        if not self.clients.is_message_from_me(event['user']):
+        if 'user' in event and not self.clients.is_message_from_me(event['user']):
 
             msg_txt = event['text']
 
@@ -88,8 +226,9 @@ class RtmEventHandler(object):
                         # message starts with something like: `<@U11T7N1U3>: `... (so strip off user mention)
                         msg_txt = msg_txt[14:len(msg_txt)]
                     session_id = event['channel'] + ":" + event['user']
-                    logger.debug('Sending message: {} to wit_client actions for session_id: {}'.format(msg_txt, session_id))
-                    self.wit_client.run_actions(session_id, msg_txt, {})
+                    self.clients.send_user_typing_pause(event['channel'], sleep_time=0.0)
+                    logger.debug('Sending message: {} to wit_client actions for session_id: {} with context: {}'.format(msg_txt, session_id, self.context))
+                    self.wit_client.run_actions(session_id, msg_txt, self.context)
 
     def _is_direct_message(self, channel_id):
         return channel_id.startswith('D')
